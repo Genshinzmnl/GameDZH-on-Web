d6f0f77d162864407b5cb49d2ce5ca0362028d4b
Genshinzmnl <151982915+Genshinzmnl@users.noreply.github.com>
2024-05-04 13:58:35 +0800

    run and fly add

---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var imgSrc = "src/title.png"; // 替换为你的图片URL
+    var titleImage = document.getElementById("title");
+    
+    titleImage.style.backgroundImage = "url('" + imgSrc + "')";
+  };
